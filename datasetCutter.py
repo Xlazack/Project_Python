73fd5e88d5be080a44dc40281086e1a50e703fc8
MichalMazur00 <michal.mazur05@gmail.com>
2023-03-30 10:34:39 +0200

    Dodany Pandas i sklearn

--- datasetCutter.py
+++ datasetCutter.py
@@ -3,5 +3,5 @@ import pandas as pd
 # Read the CSV file
 df = pd.read_csv('Zgiecie nadgarstka.csv', sep=';', skiprows=3, decimal=',')
 
-# Print the DataFrame sadadlkn
+# Print the DataFrame
 print(df)
