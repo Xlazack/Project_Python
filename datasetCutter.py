6b3ce9319703233653e7f5a20aecb23efb60cb5b
Xlazack <maciek.kazalski.2000@gmail.com>
2023-03-30 11:22:34 +0200

    datasetCutter after disaster

--- datasetCutter.py
+++ datasetCutter.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-# specify the data types for columns 0 to 4
-dtypes = {'Column0': float, 'Column1': float, 'Column2': float, 'Column3': float, 'Column4': float}
-
 # Read the CSV file
-df = pd.read_csv('Zgiecie nadgarstka.csv', sep=';', skiprows=3, decimal=',', dtype=dtypes)
+df = pd.read_csv('Zgiecie nadgarstka.csv', sep=';', skiprows=4, decimal=',', low_memory=False)
 
 # Print the DataFrame
 print(df)
