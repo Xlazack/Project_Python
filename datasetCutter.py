0a528bb5228d58c8f9d112a6e83f3a424609eb8a
Xlazack <maciek.kazalski.2000@gmail.com>
2023-03-30 12:23:31 +0200

    datasetCutter after disaster

--- datasetCutter.py
+++ datasetCutter.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-
-
-
-
-
-
-
 # Read the CSV file
-df = pd.read_csv('Zgiecie nadgarstka.csv', sep=';', skiprows=4, decimal=',', low_memory=False)
+df = pd.read_csv(r'/home/xlazack/Documents/Kazalski/Zgiecie nadgarstka.csv', sep=';', skiprows=4, decimal=',', low_memory=False)
 
 # Print the DataFrame
 print(df)
+
+#defining splitting argument
+grouped = df.groupby('column_name')
+
+#splitting a file
+for name, group in grouped:
+    filename = name + '.csv'
+    group.to_csv(filename, index=False)
