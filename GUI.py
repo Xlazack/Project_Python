f12a315b1c340a15201034e3d13add03c5bdf5a2
MichalMazur00 <michal.mazur05@gmail.com>
2023-04-20 12:18:45 +0200

    GUI - wczytuje plik .csv

--- GUI.py
+++ GUI.py
@@ -1,18 +1,65 @@
-import PySimpleGUI as sg                        # Part 1 - The import
+import PySimpleGUI as sg
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
-            [sg.Input()],
-            [sg.Button('Ok')] ]
+# Funkcja rysująca wykres liniowy
+def line_plot(x, y, xlabel, ylabel):
+    plt.plot(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
 
-# Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+sg.theme('DefaultNoMoreNagging')  # Ustawienie motywu
 
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
+# Definicja layoutu okna
+layout = [
+    [sg.Text("Cześć, jestem programem badającym ruch przy pomocy analizy EMG!")],
+    [sg.Text('Wybierz plik do wgrania w fomracie .csv:')],
+    [sg.Input(), sg.FileBrowse()],
+    [sg.Button('Wgraj'), sg.Button('Anuluj')],
+]
 
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+window = sg.Window('ANALIZA EMG', layout)
 
-# Finish up by removing from the screen
-window.close()                                  # Part 5 - Close the Window
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == 'Anuluj':
+        break
+    if event == 'Wgraj':
+        filename = values[0]
+        try:
+            df = pd.read_csv(filename)
+        except:
+            sg.popup_error('Błąd odczytu pliku')
+            continue
+        sg.popup('Plik został wczytany', f'Liczba wierszy: {len(df)}', title='PLIK WGRANY PRAWIDŁOWO')
+
+        # Rysowanie wykresów
+        plt.close('all')
+        fig1, ax1 = plt.subplots()
+        line_plot(df['a'], df['b'], 'a', 'b')
+        fig2, ax2 = plt.subplots()
+        line_plot(df['a'], df['c'], 'a', 'c')
+        fig3, ax3 = plt.subplots()
+        line_plot(df['a'], df['d'], 'a', 'd')
+        fig4, ax4 = plt.subplots()
+        line_plot(df['a'], df['e'], 'a', 'e')
+
+        plt.plot(df['A'], df['B'])
+        plt.xlabel('A')
+        plt.ylabel('B')
+        plt.title('Wykres liniowy kolumny B od kolumny A')
+        plt.show()
+
+        # Wyświetlenie okien z wykresami
+        sg.popup('Wykres kolumny b od a', title='Wykres 1', location=(0, 0), keep_on_top=True)
+        plt.show(block=False, fig=fig1)
+        sg.popup('Wykres kolumny c od a', title='Wykres 2', location=(0, 100), keep_on_top=True)
+        plt.show(block=False, fig=fig2)
+        sg.popup('Wykres kolumny d od a', title='Wykres 3', location=(0, 200), keep_on_top=True)
+        plt.show(block=False, fig=fig3)
+        sg.popup('Wykres kolumny e od a', title='Wykres 4', location=(0, 300), keep_on_top=True)
+        plt.show(block=False, fig=fig4)
+
+window.close()
+
