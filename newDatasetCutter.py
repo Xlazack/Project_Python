3dcb6a1f3f852b4e2f3ecb2afef9f716db3858bf
Xlazack <maciek.kazalski.2000@gmail.com>
2023-04-27 12:17:30 +0200

    Basic Working DataCutter

--- newDatasetCutter.py
+++ newDatasetCutter.py
@@ -1,16 +1,21 @@
 import pandas as pd
+import os
 
 # Wczytaj plik and Zamień na wielki dataset
 df = pd.read_csv('Zgiecie nadgarstka.csv', sep=';', skiprows=4, decimal=',')#, low_memory=False)
-print(df)
 df = df.drop('Time,s', axis=1)
-print(df)
+# Ustal ścieżkę, gdzie pliki będą zapisywane
+outDir = "./dir"
+if not os.path.exists(outDir):
+    os.mkdir(outDir)
 # Deklaracja Tresholda
 treshold = 15
 # Deklaracja flagi i pastFlagi
 pastFlag = False
 flag = False
 name = 0
+start = 0
+end = 0
 # Iteruj po całym datasecie
 for index, rows in df.iterrows():
     #pastFlag = flag
@@ -27,19 +32,21 @@ for index, rows in df.iterrows():
         flag = False
     # Jeśli pastFlag == false and flag == true create new dataset
     if pastFlag == False and flag == True:
-        print("Ping")
+        #name of file
         name +=1
         print(name)
         filename = str(name) + ".csv"
-        #nie działa nDF = pd.DataFrame({'name':['FLEX.CAPR.U RT,%', 'BICEPS BR. RT,%', 'BRACHIORAD. RT,%',  'LAT. TRICEPS RT,%']})
-
-    #Zostało wpisywać row z df do nowego dataframu który będzie zapisany do pliku
-
-    if maxValue > treshold:
-        row = df.iloc[index]
-        nDF = nDF.append(row)
+        start = index
+    #zapisa jednego ruchu
     if pastFlag == True and flag == False:
-        nDF.to_csv(filename, sep=';')
+        end = index
+        nDF = df.loc[start:end].copy()
+        new_index = []
+        for i in range(0,nDF.size):
+            new_index.append(i)
+        nDF.reset_index(drop=True, inplace=True)
+        fullname = os.path.join(outDir, filename)
+        nDF.to_csv(fullname, sep=';')
 #print(index)
 # Jeśli flag == true and MV > treshold -> append new row in dataset
 
